a577ba9a363d8f5902a4a671bd1da121e25fb657
Alihasanmu <30725063+Alihasanmu@users.noreply.github.com>
2017-08-09 19:26:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var img=document.getElementById("madi");
+var margin=0;
+function moveRight()
+{
+    margin=margin+1;
+    img.style.marginLeft=margin+'px';
+}
+img.onclick=setInterval(moveRight,50);
