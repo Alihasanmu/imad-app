6c6533d9a38d09ac7f7750b05646d483e43924b9
Alihasanmu <30725063+Alihasanmu@users.noreply.github.com>
2017-08-10 16:06:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ button.onclick=function()
     }
         }
     };
-  request.open('GET','http://http://alihasanmulji.imad.hasura-app.io/counter',true);
+  request.open('GET','http://alihasanmulji.imad.hasura-app.io/counter',true);
   request.send(null);
   
 };
