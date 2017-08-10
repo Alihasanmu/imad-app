2dde41740e093e26adb8a2ad3580ed65db73d0d8
Alihasanmu <30725063+Alihasanmu@users.noreply.github.com>
2017-08-10 14:23:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var articles = {
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
-    res.send(counter);
+    res.send(counter.toString());
 });
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
