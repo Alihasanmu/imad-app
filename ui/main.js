72066ada05eaaa440cd9328d0ea3a29833a843a0
Alihasanmu <30725063+Alihasanmu@users.noreply.github.com>
2017-08-10 16:02:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,6 @@ img.onclick=function()
 setInterval(moveRight,50);
 };
 var button=document.getElementById("counter");
-var counter=0;
 button.onclick=function()
 {
     var request= new XMLHttpRequest();
