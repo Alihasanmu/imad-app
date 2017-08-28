64d869c9ed43ea89f01e94fe40630b351d37db1a
Alihasanmu <30725063+Alihasanmu@users.noreply.github.com>
2017-08-28 23:19:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,14 @@ button.onclick=function()
   var submit=document.getElementById("submit_btn");
   submit.onclick=function()
   {
-      
+      var names=['name1','name2','name3'];
+      var list="";
+      for(i=0;i<names.length;i++)
+      {
+          list+='<li>'+names[i]+'</li>';
+      }
+      var ul=document.getElementById("namesList");
+      ul.innerHTML=list;
   };
   
 };
