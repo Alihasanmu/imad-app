4aed5dc6df47b8dd2bbef23523c86bbe333cfd8f
Alihasanmu <30725063+Alihasanmu@users.noreply.github.com>
2017-09-10 16:38:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/test-db', function(req,res)
         res.status(500).send(err.toString());    
         }else
         {
-          res.send(JSON.stringify());  
+          res.send(JSON.stringify(result));  
         }
     });
 });
